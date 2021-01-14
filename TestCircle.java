13887db17f4245b732db0dc0d2e59fc8694a02d9
Yumibk82 <70868707+Yumibk82@users.noreply.github.com>
2021-01-14 15:19:33 +0700

    Add files via upload

---
+++ TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle=new Circle();
+        circle.getRadius();
+        double result=circle.getArea(5);
+        System.out.println(result);
+    }
+}
